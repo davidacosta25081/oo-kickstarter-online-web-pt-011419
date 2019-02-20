7514c519e6b31eaed50870e365d90d2c97802245
David Acosta <davidacosta25081@yahoo.com>
2019-02-20 21:43:39 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,6 +1,6 @@
-class Becker
+class Backer
 
-attr_reader :name
+attr_reader :name :backed_projects
 
   def initialize (name)
     @name = name
@@ -9,6 +9,7 @@ attr_reader :name
 
 def back_project(Project)
   @backed_projects << Project
+  
 end
 
 
